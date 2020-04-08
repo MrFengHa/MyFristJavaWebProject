41c124aa6e3ceb22efd29b33b5187474c9779f1a
MrFengHa <1109179567@qq.com>
2020-04-08 10:59:47 +0800

    监听器的初始化

--- src/listener/Demo1.java
+++ src/listener/Demo1.java
@@ -12,11 +12,11 @@ import javax.servlet.ServletContextListener;
 public class Demo1 implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        System.out.println("初始化");
+        System.out.println("ServletContextListener初始化");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        System.out.println("销毁");
+        System.out.println("ServletContextListener销毁");
     }
 }
